9ee5301cc6993f776c3e34a960afe8ac6c8802f6
jlbuild <juliabuildbot@gmail.com>
2026-01-07 18:41:14 +0000

    ORTools_jll build 9.14.0+1

--- src/ORTools_jll.jl
+++ src/ORTools_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("ORTools")
-JLLWrappers.@generate_main_file("ORTools", UUID("717719f8-c30c-5086-8f3c-70cd6a1e3a46"))
+JLLWrappers.@generate_main_file("ORTools", Base.UUID("717719f8-c30c-5086-8f3c-70cd6a1e3a46"))
 end  # module ORTools_jll
